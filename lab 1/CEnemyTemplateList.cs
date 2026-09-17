a8e7964e28b9c8cd9b4eee10d7c2e5bd2b3d88d9
Инна <nidergaysi456@gmail.com>
2026-09-17 11:47:43 +0700

    Добавление списка противников

---
+++ lab 1/CEnemyTemplateList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_1
+{
+    public class CEnemyTemplateList
+    {
+        //Список противников из класса CEnemyTemplate
+        List<CEnemyTemplate> enemies;
+        public CEnemyTemplateList()
+        {
+            enemies = new List<CEnemyTemplate>();
+        }
+    }
+}
