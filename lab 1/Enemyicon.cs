c7e636dcb51e67ff6b74b6e04f71b44256dcf175
Инна <nidergaysi456@gmail.com>
2026-09-23 19:54:06 +0700

    Добавлен класс для хранения данных об иконке

---
+++ lab 1/Enemyicon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_1
+{
+    internal class Enemyicon
+    {
+        public string Name { get; set; }
+
+        public string ImagePath { get; set; }
+
+    }
+}
